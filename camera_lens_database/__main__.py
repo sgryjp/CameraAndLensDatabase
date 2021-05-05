0197ef5242cf11ab9e55422389f6bb2347d58837
Suguru Yamamoto <sgryjp@gmail.com>
2021-05-05 15:38:45 +0900

    feat: Add cache subcommand to show/purge cache

--- camera_lens_database/__main__.py
+++ camera_lens_database/__main__.py
@@ -1,12 +1,19 @@
 import logging
+import shutil
+from enum import Enum
 
 import pandas as pd
 import typer
 
-from . import nikon_z
+from . import cache_root, nikon_z
 
 _logger = logging.getLogger(__name__)
 app = typer.Typer()
+
+
+class CacheAction(str, Enum):
+    info = "info"
+    purge = "purge"
 
 
 @app.command()
@@ -25,6 +32,22 @@ def fetch():
         print(df)
     except Exception:
         _logger.exception("")
+
+
+@app.command()
+def cache(action: CacheAction = typer.Argument(CacheAction.info)):
+    """Do cache related operation."""
+
+    def print_error(func, path, exc_info):
+        _, ex, _ = exc_info
+        msg = f"cannot remove '{path}': {str(ex)}"
+        _logger.warning(msg)
+
+    if action == CacheAction.info:
+        print(cache_root.absolute())
+    elif action == CacheAction.purge:
+        if cache_root.exists():
+            shutil.rmtree(cache_root, onerror=print_error)
 
 
 if __name__ == "__main__":
